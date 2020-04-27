b6745a871a3c17a73915d596ab7ae079296182f6
megafly-25 <luis-rc_13@hotmail.com>
2020-04-27 16:01:53 -0500

    block ember

--- megafly/settings.py
+++ megafly/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'megaflydb',
-        'USER': 'megafly',
+        'USER': 'megaflydb',
         'PASSWORD':'Jorgeluis',
         'HOST':'localhost',
         'DATABASE_POST':'',
